ac31985ce06bb663712e6de634aa90479f045a36
tomoki-yamamura <progtab17@gmail.com>
2024-06-19 22:05:55 +0800

    base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, 'min-h-dvh')}>
+        <header className="h-16 border-b px-6 flex items-center gap-3">
+          <Button asChild variant={"ghost"} className="font-bold text-xl">
+            <Link href="/">Header</Link>
+          </Button>
+          <Button asChild variant={"ghost"} className="font-bold text-xl">
+            <Link href="/about">About</Link>
+          </Button>
+        </header>
+        {children}
+        <footer className="h-16 sticky top-full border-t px-6 flex items-center">footer</footer>
+      </body>
     </html>
   );
 }
