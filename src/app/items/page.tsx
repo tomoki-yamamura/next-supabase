397bc94b4bf3a8a7bee7e39d294cee38e4251f64
tomoki-yamamura <progtab17@gmail.com>
2024-06-21 00:48:37 +0800

    lists grid

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -4,13 +4,15 @@ import { getItems } from "../data/item"
 export default async function page() {
   const items = await getItems();
   return (
-    <div>
+    <div className="p-6">
       <h1 className="font-bold text-2xl mb-6">Lists</h1>
 
-      <div className="space-y-2">
+      <div className="grid grid-cols-2 gap-2">
         {items?.map(item => (
           <div key={item.id} className="border-2 p-2 rounded-lg">
-            {item.name} / {item.amount}
+            <div>
+              {item.name} / {item.amount.toLocaleString()}円
+            </div>
           </div>
         ))}
       </div>
